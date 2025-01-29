ad768206c6107376f5e3a3e4ad026439ae949bb1
Dylserker <levant.dylan@gmail.com>
2025-01-29 13:48:06 +0100

    oublie de push x)

---
+++ hooks/useFetchQuery.ts
@@ -0,0 +1,103 @@
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+
+type PaginatedResults<T> = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: T[];
+};
+
+type API = {
+  "/pokemon?limit=21": PaginatedResults<{
+    name: string;
+    url: string;
+  }>;
+  "/pokemon-species/:id": {
+    flavor_text_entries: {
+      flavor_text: string;
+      language: {
+        name: string;
+      };
+    }[];
+  };
+  "/pokemon/:id": {
+    id: number;
+    name: string;
+    url: string;
+    weight: number;
+    height: number;
+    moves: { move: { name: string } }[];
+    stats: {
+      base_stat: number;
+      stat: {
+        name: string;
+      };
+    }[];
+    cries: {
+      latest: string;
+    };
+    types: {
+      type: {
+        name: string;
+      };
+    }[];
+  };
+};
+
+type ItemOf<T> = T extends (infer I)[] ? I : never;
+
+export type APIResult<T extends keyof API> = API[T] extends { results: infer R }
+  ? ItemOf<R>
+  : never;
+
+export function useFetchQuery<T extends keyof API>(
+  url: T,
+  params?: Record<string, string | number>,
+) {
+  const localUrl = Object.entries(params ?? {}).reduce(
+    (acc, [key, value]) => acc.replace(":" + key, value.toString()),
+    url as string,
+  );
+  return useQuery({
+    queryKey: [localUrl],
+    queryFn: async () => {
+      // await wait(1000);
+      return fetch("https://pokeapi.co/api/v2" + localUrl, {
+        headers: {
+          Accept: "application/json",
+        },
+      }).then((r) => r.json()) as Promise<API[T]>;
+    },
+  });
+}
+
+export function useInfiniteFetchQuery<T extends keyof API>(
+  url: T,
+  params?: Record<string, string | number>,
+) {
+  const localUrl = Object.entries(params ?? {}).reduce(
+    (acc, [key, value]) => acc.replace(":" + key, value.toString()),
+    url as string,
+  );
+  return useInfiniteQuery({
+    queryKey: [localUrl],
+    initialPageParam: "https://pokeapi.co/api/v2" + localUrl,
+    queryFn: async ({ pageParam }) => {
+      await wait(1000);
+      return fetch(pageParam, {
+        headers: {
+          Accept: "application/json",
+        },
+      }).then((r) => r.json()) as Promise<API[T]>;
+    },
+    getNextPageParam: (lastPage) => {
+      if (!("next" in lastPage)) {
+        throw new Error("Unpaginated result");
+      }
+      return lastPage.next;
+    },
+  });
+}
+const wait = (duration: number) => {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+};
